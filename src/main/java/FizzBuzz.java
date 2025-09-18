bffb8a8cf4f172f26f55cd3068eaca111c151656
maddyb129 <maddyball814@gmail.com>
2025-09-18 11:26:14 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -12,20 +12,20 @@ public class FizzBuzz
     public static void main(String[] args)
     {
         // TODO
-        int num = 0;
-        for (int i=0; i<=100; i++)
+        int num = 1;
+        for (int i=1; i<=100; i++)
         {
             if (num % 15 == 0)
             {
-                System.out.println(num + "FizzBuzz");
+                System.out.println("FizzBuzz");
             }
             else if (num % 3 == 0)
             {
-                System.out.println(num + "Fizz");
+                System.out.println("Fizz");
             }
             else if (num % 5 == 0)
             {
-                System.out.println(num + "Buzz");
+                System.out.println("Buzz");
             }
             else
             {
