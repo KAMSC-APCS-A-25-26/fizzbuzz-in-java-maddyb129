3bee97a69fba07e2b6ffda581163b6c05ca9123d
maddyb129 <maddyball814@gmail.com>
2025-09-18 11:23:32 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,4 +1,5 @@
-public class FizzBuzz {
+public class FizzBuzz
+{
     /**
      * Print the FizzBuzz sequence for 1..100 to standard output.
      * Rules:
@@ -8,7 +9,32 @@ public class FizzBuzz {
      * - Otherwise -> the number itself
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO
+        int num = 1;
+        for (int i=1; i<=100; i++)
+        {
+            if (num % 15 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(num);
+            }
+            num = num + 1;
+
+        }
+
     }
 }
+
